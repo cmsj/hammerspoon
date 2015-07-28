1c02d7002c9a3d32b04a347302d64fa44a992a90
Chris Jones <cmsj@tenshu.net>
2015-07-28 13:20:54 +0100

    Flesh out mkCertTemplate.sh a little

--- extensions/remoteapp/mkCertTemplate.sh
+++ extensions/remoteapp/mkCertTemplate.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+echo "== Generating a private key and using it to sign an x509:"
 openssl req -x509 -newkey rsa:2048 -outform DER -out cert.cer -config openssl.conf -set_serial 106
+echo ""
+echo "== Dumping ASN.1 structure of cert.cer:"
 openssl asn1parse -inform DER -in cert.cer -dump -i
+echo ""
+echo "== Hexdump of cert.cer:"
+hexdump -e '"\t" 16/1 "0x%02x, " "\n"' cert.cer
+echo ""
+echo "== Done"
